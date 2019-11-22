a157ddcfd3def2c8c2ccbf81a487ef2adf2523b4
LeannaMulv <40302225+LeannaMulv@users.noreply.github.com>
2019-11-22 14:37:00 -0500

    Add files via upload

---
+++ Arduino_to_pi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import serial
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+while 1: 
+    if(ser.in_waiting >0):
+        line = ser.readline()
+        print(line)
+
